c6ca7e41585ddc909c09e1faf083b3997554e2b0
mhshuvro <mainul.hasan.shuvro@gmail.com>
2019-05-13 19:23:23 +0200

    Added basic routing with better folder structure, added bootstrap, jquery with dependencies

---
+++ career-tinder/src/components/authentication/registration.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Registration extends Component {
+    render() {
+      return (
+        <h1>This is Registration page</h1>
+      );
+    }
+  }
+  
+  export default Registration;
