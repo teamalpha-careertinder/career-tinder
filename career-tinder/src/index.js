0c57c243e49ab59af555162eb6de16ada85b78e1
Anny Zahid <anny.zahid@stud.uni-bamberg.de>
2019-05-13 23:26:09 +0000

    Update index.js

--- career-tinder/src/index.js
+++ career-tinder/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-=======
->>>>>>> 68534c00b28c4fceb3d908268fcc5847d04fb129
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-<<<<<<< HEAD
   
     <App />,document.getElementById('root')
-=======
-  <App />,
-  document.getElementById('root')
->>>>>>> 68534c00b28c4fceb3d908268fcc5847d04fb129
 );
