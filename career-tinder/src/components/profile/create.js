8396278ab37388ab36b865684e37302ae7b0958e
Mainul Hasan Shuvro <mainul-hasan.shuvro@stud.uni-bamberg.de>
2019-05-15 03:57:00 +0000

    Applied material design to profile creation page

--- career-tinder/src/components/profile/create.js
+++ career-tinder/src/components/profile/create.js
@@ -1,56 +1,58 @@
 import React, { Component } from 'react';
+import { MDBInput, MDBIcon, MDBBtn } from "mdbreact";
 
 class CreateProfile extends Component {
     render() {
       return (
         <div className="profile-form-wrapper">        
           <div className="card border-info mb-3">
-            <div className="card-header">Create new profile</div>
+            <div className="card-header">
+              <MDBIcon icon="user" className="mr-1" /> Create new profile
+            </div>
             <div className="card-body text-info">
               <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li className="nav-item">
-                  <a className="nav-link active" id="pills-job-seeker-tab" data-toggle="pill" href="#pills-job-seeker" role="tab" aria-controls="pills-job-seeker" aria-selected="true">Job Seeker</a>
+                  <a className="nav-link active" id="pills-job-seeker-tab" data-toggle="pill" href="#pills-job-seeker" role="tab" aria-controls="pills-job-seeker" aria-selected="true">
+                    <MDBIcon icon="users" className="mr-1" /> Job Seeker
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" id="pills-company-tab" data-toggle="pill" href="#pills-company" role="tab" aria-controls="pills-company" aria-selected="false">Company</a>
+                  <a className="nav-link" id="pills-company-tab" data-toggle="pill" href="#pills-company" role="tab" aria-controls="pills-company" aria-selected="false">
+                    <MDBIcon icon="user-tie" className="mr-1" /> Employer
+                  </a>
                 </li>
               </ul>
               <div className="tab-content" id="pills-tabContent">
                 <div className="tab-pane fade show active" id="pills-job-seeker" role="tabpanel" aria-labelledby="pills-job-seeker-tab">
                   <form className="profile-form">
-                    <div className="form-row">
+                    <div className="row">
                       <div className="col-md-4 col-sm-12">
                         <div className="form-group">
-                          <label for="name">Name:</label>
-                          <input type="text" className="form-control" id="name" placeholder="John Doe" />
+                          <MDBInput label="Name" outline type="text" icon="pencil-alt" />
                         </div>
                       </div>
                       <div className="col-md-4 col-sm-12">
                         <div className="form-group">
-                          <label for="dob">Date of Birth:</label>
-                          <input type="text" className="form-control" id="dob" placeholder="dd.mm.yyyy" />
+                          <MDBInput label="Date of Birth" outline type="text" icon="calendar-alt" />
                         </div>
                       </div>
                       <div className="col-md-4 col-sm-12">
                         <div className="form-group">
-                          <label for="phone">Phone:</label>
-                          <input type="text" className="form-control" id="phone" placeholder="01234567890" />
+                          <MDBInput label="Phone" outline icon="mobile-alt" type="text" />
                         </div>
                       </div>
                     </div>
-                    <div className="form-row">            
+                    <div className="row">            
                       <div className="col-sm-12">
                         <div className="form-group">
-                          <label for="address">Address:</label>
-                          <textarea className="form-control" id="address" rows="2"></textarea>
+                          <MDBInput label="Address" outline icon="address-card" type="textarea" rows="1" />
                         </div>
                       </div>
                     </div>
-                    <div className="form-row">
+                    <div className="row">
                       <div className="col-md-6 col-sm-12">
                         <div className="form-group">
-                          <label for="skills">Skills:</label>
-                          <input type="text" className="form-control" id="skills" placeholder="Skills" />
+                          <MDBInput label="Skills" outline icon="walking" type="text" />
                         </div>
                       </div>
                       <div className="col-md-6 col-sm-12">
@@ -65,7 +67,7 @@ class CreateProfile extends Component {
                         </div>
                       </div>
                     </div>   
-                    <div className="form-row">
+                    <div className="row">
                       <div className="col-md-6 col-sm-12">
                         <div className="form-group">
                           <label for="work_experience">Work Experience:</label>
@@ -79,66 +81,63 @@ class CreateProfile extends Component {
                         </div>
                       </div>
                     </div>    
-                    <div className="form-row">
+                    <div className="row">
                       <div className="col-sm-12">
-                        <button type="button" className="btn btn-outline-info float-right">Create Job Seeker</button>
+                        <MDBBtn outline color="info" className="float-right">
+                          <MDBIcon icon="plus" className="mr-1" /> Create Job Seeker
+                        </MDBBtn>
                       </div>
                     </div>      
                   </form>  
                 </div>
                 <div className="tab-pane fade" id="pills-company" role="tabpanel" aria-labelledby="pills-company-tab">
                   <form className="profile-form">
-                      <div className="form-row">
+                      <div className="row">
                         <div className="col-md-6 col-sm-12">
                           <div className="form-group">
-                            <label for="company_name">Company Name:</label>
-                            <input type="text" className="form-control" id="company_name" placeholder="John Doe" />
+                            <MDBInput label="Employer Name" outline icon="pencil-alt" type="text" />
                           </div>
                         </div>
                         <div className="col-md-6 col-sm-12">
                           <div className="form-group">
-                            <label for="industry">Industry:</label>
-                            <input type="text" className="form-control" id="industry" placeholder="dd.mm.yyyy" />
+                            <MDBInput label="Industry" type="text" outline icon="industry" />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">
+                      <div className="row">
                         <div className="col-md-4 col-sm-12">
                           <div className="form-group">
-                            <label for="contact_name">Contact Name:</label>
-                            <input type="text" className="form-control" id="contact_name" placeholder="Jane Doe" />
+                            <MDBInput label="Contact Name" type="text" outline icon="pencil-alt" />
                           </div>
                         </div>
                         <div className="col-md-4 col-sm-12">
                           <div className="form-group">
-                            <label for="contact_email">Email:</label>
-                            <input type="email" className="form-control" id="contact_email" placeholder="john.doe@email.com" />
+                            <MDBInput label="Contact Email" type="email" outline icon="envelope" />
                           </div>
                         </div>
                         <div className="col-md-4 col-sm-12">
                           <div className="form-group">
-                            <label for="contact_phone">Phone:</label>
-                            <input type="text" className="form-control" id="contact_phone" placeholder="01234567890" />
+                            <MDBInput label="Contact Phone" type="text" outline icon="mobile-alt" />
                           </div>
                         </div>
                       </div>
-                      <div className="form-row">            
+                      <div className="row">            
                         <div className="col-md-6 col-sm-12">
                           <div className="form-group">
-                            <label for="company_address">Address:</label>
-                            <textarea className="form-control" id="company_address" rows="2"></textarea>
+                            <MDBInput label="Employer Address" type="textarea" rows="1" outline icon="address-card" />
                           </div>
                         </div>
                         <div className="col-md-6 col-sm-12">
                           <div className="form-group">
-                            <label for="company_description">Description:</label>
-                            <textarea className="form-control" id="company_description" rows="2"></textarea>
+                            <MDBInput label="Employer Description" type="textarea" rows="1" outline icon="comment-alt" />
                           </div>
                         </div>
                       </div>      
-                      <div className="form-row">
+                      <div className="row">
                         <div className="col-sm-12">
-                          <button type="button" className="btn btn-outline-info float-right">Create Company</button>
+                          <MDBBtn outline color="info" className="float-right">
+                            <MDBIcon icon="plus" className="mr-1" /> Create Employer
+                          </MDBBtn>
                         </div>
                       </div>        
                     </form>  
