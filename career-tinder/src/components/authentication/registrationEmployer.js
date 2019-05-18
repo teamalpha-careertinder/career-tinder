53d1653063b56d44d03c39fbd82af6824ff693ab
Monojit Goon <monojit.goon@stud.uni-bamberg.de>
2019-05-18 18:45:22 +0200

    Signup with firebase auth resolved

--- career-tinder/src/components/authentication/registrationEmployer.js
+++ career-tinder/src/components/authentication/registrationEmployer.js
@@ -1,34 +1,43 @@
 import React from "react";
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import {
-  MDBMask, MDBRow, MDBCol, MDBIcon, MDBBtn, MDBView, MDBContainer, MDBCard, MDBCardBody, MDBInput
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput
 } from "mdbreact";
 import "./registrationEmployer.css";
-
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { signUpAsEmployer } from "../../store/actions/authActions";
 
 class RegistrationEmployer extends React.Component {
-
   state = {
-    collapseID: ""
+    email: "",
+    password: "",
+    companyname: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.signUpAsEmployer(this.state);
   };
 
-  toggleCollapse = collapseID => () =>
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-    }));
-
   render() {
-    const overlay = (
-      <div id="sidenav-overlay" style={{ backgroundColor: "transparent" }} onClick={this.toggleCollapse("navbarCollapse")} />
-    );
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to="/feed" />
     return (
       <div id="classicformpage">
-        <Router>
-          <div>
-            {this.state.collapseID && overlay}
-          </div>
-        </Router>
-
         <MDBView>
           <MDBMask className="d-flex justify-content-center align-items-center gradient">
             <MDBContainer>
@@ -39,38 +48,67 @@ class RegistrationEmployer extends React.Component {
                   </h1>
                   <hr className="hr-light" />
                   <h6 className="mb-4">
-                    Welcome to Career Tinder website.
-                      This website is desgined for the companies which are looking
-                      to hire new employees, as well as people how are looking for job.
-                      To use the offered services of the web, please login with your account
-                      or if you don't have an account yet, please click the signup button to register
-                </h6>
+                    Welcome to Career Tinder website. This website is desgined
+                    for the companies which are looking to hire new employees,
+                    as well as people how are looking for job. To use the
+                    offered services of the web, please login with your account
+                    or if you don't have an account yet, please click the signup
+                    button to register
+                  </h6>
                   <MDBBtn outline color="white">
                     Learn More
-                </MDBBtn>
+                  </MDBBtn>
                 </div>
                 <MDBCol md="6" xl="5" className="mb-4">
                   <MDBCard id="classic-card">
                     <MDBCardBody className="z-depth-2 white-text">
                       <h3 className="text-center">
                         <MDBIcon icon="user" /> Employer
-                    </h3>
+                      </h3>
                       <hr className="hr-light" />
-
-                      <MDBInput label="Company Name" icon="user" className="white-text" />
-                      <MDBInput label="Email" icon="envelope" className="white-text" />
-                      <MDBInput label="Password" icon="lock" type="password" className="white-text" />
+                      <form onSubmit={this.handleSubmit}>
+                      <MDBInput
+                       id="companyname"
+                        label="Company Name"
+                        className="white-text"
+                        onChange={this.handleChange}
+                      />
+                      <MDBInput
+                       id="email"
+                        label="Email"
+                        icon="envelope"
+                        className="white-text"
+                        autoComplete="false"
+                        onChange={this.handleChange}
+                      />
+                      <MDBInput
+                       id="password"
+                        label="Password"
+                        icon="lock"
+                        type="password"
+                        className="white-text"
+                        autocomplete="new-password"
+                        onChange={this.handleChange}
+                      />
 
                       <div className="text-center mt-4 black-text">
-                        <MDBBtn color="indigo">Sign Up</MDBBtn>
-                      </div>
+                        <MDBBtn color="indigo" type="submit">Sign Up</MDBBtn>
+                        <div className="center red-text">
+                            {authError ? <p>{authError}</p> : null}
+                          </div>
+                        </div>
+                      </form>
 
-                      <div className="text-center mt-4">    
+                      <div className="text-center mt-4">
                         <hr className="hr-light" />
-                        <div className="text-center d-flex justify-content-center white-label"> 
-                          <NavLink className="red-text" to="/registration/jobseeker">
-                            <i className="fas fa-user-plus"></i> Oops! I'm a Job Seeker!
-                          </NavLink>  
+                        <div className="text-center d-flex justify-content-center white-label">
+                          <NavLink
+                            className="red-text"
+                            to="/registration/jobseeker"
+                          >
+                            <i className="fas fa-user-plus" /> Oops! I'm a Job
+                            Seeker!
+                          </NavLink>
                         </div>
                       </div>
                     </MDBCardBody>
@@ -80,10 +118,25 @@ class RegistrationEmployer extends React.Component {
             </MDBContainer>
           </MDBMask>
         </MDBView>
-
       </div>
-    );
+    )
   }
 }
 
-export default RegistrationEmployer;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signUpAsEmployer: creds => dispatch(signUpAsEmployer(creds))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RegistrationEmployer);
--- career-tinder/src/components/feedboard/feedboard.js
+++ career-tinder/src/components/feedboard/feedboard.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import { MDBMask, MDBRow, MDBView, MDBContainer } from "mdbreact";
+import { Link } from "react-router-dom";
 
 class Feedboard extends Component {
   render() {
@@ -10,9 +12,26 @@ class Feedboard extends Component {
     if (!auth.uid) return <Redirect to="/login" />;
 
     return (
-      <div className="feedboard container">
-        Welcome to your career-tinder profile. Please edit your profile to have
-        your profile complete. Your job feed is coming soon.
+      <div className="container">
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
+                  <h1 className="h1-responsive font-weight-bold">
+                    Career Tinder{" "}
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    Welcome to your career-tinder profile. Please
+                    <Link to="/profile/create"> edit your profile </Link>
+                    to have your profile complete. Your job feed is coming soon.
+                  </h6>
+                </div>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
       </div>
     );
   }
