1d4dad1d81b58d48d1807cee808352cf9fca6eb6
Tanvir Ahmed Siddique <tanvir-ahmed.siddique@stud.uni-bamberg.de>
2019-05-21 23:38:10 +0000

    Update feedboard.js

--- career-tinder/src/components/feedboard/feedboard.js
+++ career-tinder/src/components/feedboard/feedboard.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
-import {  MDBRow } from "mdbreact";
+import { MDBView, MDBRow, MDBMask, MDBContainer } from "mdbreact";
 import { Link } from "react-router-dom";
 
 class Feedboard extends Component {
