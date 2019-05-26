c2c4446e7a6b65bc423378914e0ecf1617c4ad24
Mainul Hasan Shuvro <mainul-hasan.shuvro@stud.uni-bamberg.de>
2019-05-26 13:48:45 +0200

    Added the UI for adding multiple work experiences

--- career-tinder/src/components/profile/editJobSeekerProfile.js
+++ career-tinder/src/components/profile/editJobSeekerProfile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import DatePicker from "react-datepicker";
 import Select from 'react-select';
-import { MDBInput, MDBIcon, MDBBtn } from "mdbreact";
+import { MDBInput, MDBBtn } from "mdbreact";
 import './profile.css';
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
-import { Alert } from 'reactstrap';
+import { Alert, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import EmailVerification from "../authentication/emailVerification";
 import { editJobSeekerProfile } from '../../store/actions/profileAction';
 
@@ -40,11 +40,15 @@ class EditJobSeekerProfile extends React.Component {
         startDOBDate: '',
         startFromDate: '',
         startToDate: '',
-        visible: false
+        visible: false,
+        modal: false,
+        workExperiences: []
       };
       this.handleDOBDateChange = this.handleDOBDateChange.bind(this);
       this.handleFromDateChange = this.handleFromDateChange.bind(this);
       this.handleToDateChange = this.handleToDateChange.bind(this);
+      this.toggle = this.toggle.bind(this);
+      this.handleWEDelete = this.handleWEDelete.bind(this);
     }
   
     handleDOBDateChange(date) {
@@ -111,6 +115,32 @@ class EditJobSeekerProfile extends React.Component {
       this.props.editJobSeekerProfile(this.state); 
     }
 
+    handleWExperienceSubmit = (e) => {
+        e.preventDefault();
+        const { workExperiences } = this.state;
+        const newWExperience = {
+            companyName: e.target.company_name.value,
+            jobTitle: e.target.job_title.value,
+            workingFrom: e.target.working_from.value,
+            workedTo: e.target.worked_to.value,
+            jobDescription: e.target.job_description.value,
+            jobType: e.target.job_type.value,
+        };
+        newWExperience.companyName !== '' && this.setState({
+            workExperiences: [...this.state.workExperiences, newWExperience]
+        });
+    }
+
+    handleWEDelete(experience) {
+        const newWorkExperiences = this.state.workExperiences.filter(workExperience => {
+            return workExperience !== experience;
+        });
+
+        this.setState({
+            workExperiences: [...newWorkExperiences]
+        });
+    }
+
     onShowAlert = ()=>{
       this.setState({visible:true},()=>{
         window.setTimeout(()=>{
@@ -119,17 +149,91 @@ class EditJobSeekerProfile extends React.Component {
       });
     }
 
+    toggle() {
+        this.setState(prevState => ({
+          modal: !prevState.modal
+        }));
+    }
+
     render() {
       const { selectedSkills } = this.state;
       const { selectedLanguages } = this.state;
-      const { auth } = this.props;
-       
-          
+      const { auth } = this.props;          
 
       if (!auth.uid) return <Redirect to="/login" />;
       return (
         <div className="job-seeker-profile">
           <Alert color="success" isOpen={this.state.visible}><i class="fas fa-check"></i> Profile updated!</Alert>
+          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+            <ModalHeader toggle={this.toggle}>Your Work Experience</ModalHeader>
+            <ModalBody>
+                <form className="work-experience-form text-info" onSubmit={this.handleWExperienceSubmit}>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="form-group">
+                                <label htmlFor="work_experience">Work Experience:</label>
+                                <div className="row">
+                                    <div className="col-md-6 col-sm-12">
+                                        <MDBInput id="companyName" label="Company name" type="text" icon="pencil-alt" name="company_name" onChange={this.handleChange} />
+                                    </div>
+                                    <div className="col-md-6 col-sm-12">
+                                        <MDBInput id="jobTitle" label="Job title" type="text" icon="pencil-alt" name="job_title" onChange={this.handleChange} />
+                                    </div>
+                                    <div className="col-md-6 col-sm-12">
+                                        <div className="form-group datepicker">
+                                            <label>From:</label>
+                                            <div className="md-form">
+                                                <i className="fas fa-calendar-alt prefix"></i>
+                                                    <DatePicker selected={this.state.startFromDate} onChange={this.handleFromDateChange} className="form-control" 
+                                                        peekNextMonth showMonthDropdown showYearDropdown dropdownMode="select" name="working_from" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 col-sm-12">
+                                        <div className="form-group datepicker">
+                                            <label>To:</label>
+                                            <div className="md-form">
+                                                <i className="fas fa-calendar-alt prefix"></i>
+                                                <DatePicker selected={this.state.startToDate} onChange={this.handleToDateChange} className="form-control" 
+                                                    peekNextMonth showMonthDropdown showYearDropdown dropdownMode="select" name="worked_to" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <MDBInput id="jobDescription" label="Job description" type="text" icon="pencil-alt" name="job_description" onChange={this.handleChange} />
+                                    </div>
+                                    <div className="col-sm-12 mb-1">
+                                        <div className="form-inline">
+                                            <div className="form-group">
+                                                <label htmlFor="job_type"className="mr-2">Job type while working there:</label>
+                                                <div className="form-check form-check-inline">
+                                                    <input className="form-check-input" type="radio" value="Full-time" checked={this.state.prevWorkJobType === 'Full-time'}  
+                                                        onChange={this.handleOptionChange} name="job_type" />
+                                                    <label className="form-check-label" htmlFor="job_type_ft">
+                                                        Full-time
+                                                    </label>
+                                                </div>
+                                                <div className="form-check form-check-inline">
+                                                    <input className="form-check-input" type="radio" value="Part-time" checked={this.state.prevWorkJobType === 'Part-time'}  
+                                                        onChange={this.handleOptionChange} name="job_type" />
+                                                    <label className="form-check-label" htmlFor="job_type_pt">
+                                                        Part-time
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>  
+                        <div className="col-sm-12">
+                            <Button color="primary" type="submit" onClick={this.toggle}><i className="fas fa-plus"></i> Add</Button>{' '}
+                            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                        </div>                                          
+                    </div> 
+                </form>  
+            </ModalBody>
+          </Modal>
           <div className="profile-form-wrapper">        
             <div className="card border-info card-container">
               <div className="card-header">
@@ -226,69 +330,24 @@ class EditJobSeekerProfile extends React.Component {
                             <MDBInput id="salaryRange" label="Salary range" icon="euro-sign" type="text" onChange={this.handleChange} />
                           </div>
                         </div>  
-                      </div>   
-                      <div className="row">
                         <div className="col-sm-12">
-                          <div className="form-group">
-                            <label htmlFor="work_experience">Work Experience:</label>
-                            <div className="row">
-                              <div className="col-md-6 col-sm-12">
-                                <MDBInput id="companyName" label="Company name" type="text" icon="pencil-alt" onChange={this.handleChange} />
-                              </div>
-                              <div className="col-md-6 col-sm-12">
-                                <MDBInput id="jobTitle" label="Job title" type="text" icon="pencil-alt" onChange={this.handleChange} />
-                              </div>
-                              <div className="col-md-6 col-sm-12">
-                                <div className="form-group datepicker">
-                                  <label>From:</label>
-                                  <div className="md-form">
-                                    <i className="fas fa-calendar-alt prefix"></i>
-                                    <DatePicker selected={this.state.startFromDate} onChange={this.handleFromDateChange} className="form-control" 
-                                      peekNextMonth showMonthDropdown showYearDropdown dropdownMode="select" />
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="col-md-6 col-sm-12">
-                                <div className="form-group datepicker">
-                                  <label>To:</label>
-                                  <div className="md-form">
-                                    <i className="fas fa-calendar-alt prefix"></i>
-                                    <DatePicker selected={this.state.startToDate} onChange={this.handleToDateChange} className="form-control" 
-                                      peekNextMonth showMonthDropdown showYearDropdown dropdownMode="select" />
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="col-sm-12">
-                                <MDBInput id="jobDescription" label="Job description" type="text" icon="pencil-alt" onChange={this.handleChange} />
-                              </div>
-                              <div className="col-sm-12 mb-1">
-                                <div className="form-inline">
-                                  <div className="form-group">
-                                    <label htmlFor="job_type"className="mr-2">Job type while working there:</label>
-                                    <div className="form-check form-check-inline">
-                                      <input className="form-check-input" type="radio" value="Full-time" checked={this.state.prevWorkJobType === 'Full-time'}  onChange={this.handleOptionChange} />
-                                      <label className="form-check-label" htmlFor="job_type_ft">
-                                        Full-time
-                                      </label>
-                                    </div>
-                                    <div className="form-check form-check-inline">
-                                      <input className="form-check-input" type="radio" value="Part-time" checked={this.state.prevWorkJobType === 'Part-time'}  onChange={this.handleOptionChange} />
-                                      <label className="form-check-label" htmlFor="job_type_pt">
-                                        Part-time
-                                      </label>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="col-sm-12">
-                                <MDBBtn color="success" className="btn-sm">
-                                  <MDBIcon icon="plus" className="mr-1" /> Add work experience
-                                </MDBBtn>
-                              </div>
+                            <div id="work_experiences">
+                                {
+                                    this.state.workExperiences.map((workExperience,i) => {
+                                        return (
+                                            <span key={`work-experience-${i}`} className="work-experiences-tag badge badge-dark ml-1">
+                                                {workExperience.companyName}
+                                                <i className="fas fa-times ml-3" onClick={(e) => this.handleWEDelete(workExperience)}></i>                                          
+                                            </span>
+                                        )
+                                    })
+                                }
                             </div>
-                          </div>
-                        </div>                                            
-                      </div>    
+                        </div>
+                        <div className="col-sm-12">
+                            <Button color="danger" onClick={this.toggle}>Add Working Experiences</Button>
+                        </div>
+                      </div>                          
                       <div className="row">
                         <div className="col-sm-12">
                           <MDBBtn color="primary" className="float-right" type="submit" onClick={()=>{this.onShowAlert()}}>
