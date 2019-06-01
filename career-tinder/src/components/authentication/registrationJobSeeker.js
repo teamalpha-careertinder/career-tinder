6a72d349cf3b7acd1de44a62caca96ad5b12b296
Abdullah Al Noman <abdullah-al.noman@stud.uni-bamberg.de>
2019-06-01 16:23:33 +0200

    Test code for Formik validation

--- career-tinder/src/components/authentication/registrationJobSeeker.js
+++ career-tinder/src/components/authentication/registrationJobSeeker.js
@@ -5,28 +5,25 @@ import * as ROUTES from "../../constants/routes";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { signUpAsJobSeeker } from "../../store/actions/authActions";
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 class RegistrationJobSeeker extends React.Component {
   state = {
+    name: "",
     email: "",
     password: "",
-    confirm_password: "",
-    FullName: ""
+    confirm_password: ""
   };
 
-  changeHandler = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  
 
-  /* handleChange = e => {
+  handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
     });
-  }; */
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.signUpAsJobSeeker(this.state);
   };
+  
 
   render() {
     const { auth, authError } = this.props;
@@ -47,70 +44,105 @@ class RegistrationJobSeeker extends React.Component {
           <MDBCardBody className="z-depth-2 gradient-text text-info">
             <div className="row">
               <div className="col-md-6 col-sm-12">
-                <form onSubmit={this.handleSubmit}>
 
-                <MDBInput
-                   value={this.state.name}
-                    name="name"
-                    onChange={this.changeHandler}
-                    id="Name"
-                    label="Full Name"
-                    icon="pencil-alt"
-                    type="text"
-                    className="form-control"
-                    required
-                    //onChange={this.handleChange}
-                    />
+              <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                    password: '',
+                    confirmpassword: ''
+                }}
 
+                validationSchema={Yup.object().shape({
+                    name: Yup.string()
+                        .required('Name is required'),
+                    email: Yup.string()
+                        .email('Email is invalid')
+                        .required('Email is required'),
+                    password: Yup.string()
+                        .min(8, 'Password must be at least 8 characters')
+                        .required('Password is required'),
+                    confirmpassword:  Yup.string()
+                        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+                        .required('Confirm Password is required')
+                })}
+
+                onSubmit={fields => {
+                    alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
+                }}
+
+                render={({ errors, status, touched }) => (
+                <Form>
+
+                  <div>
+                    <div className="md-form">
+                    <i className="fa fa-pencil-alt prefix"></i>
+                    <Field
+                    //value={this.state.name}
+                      name="name"
+                      id="name"
+                      type="text"
+                      className={'form-control' + (errors.name && touched.name ? ' is-invalid' : '')}
+                      //onChange={this.handleChange}
+                      />
+                      <label class="" htmlfor="name">Your Name</label>
+                      <ErrorMessage name="name" component="div" className="invalid-feedback" />
+                    </div>
+                  </div>
+
+                  <div>
                   <MDBInput
-                    value={this.state.email}
+                    //value={this.state.email}
                          name="email"
                          label="Your email"
                          icon="envelope"
                          type="email"
                          id="email"
-                         className="form-control"
-                         onChange={this.changeHandler}
-                         required
+                         className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')}
                          autoComplete="false"
-                        //onChange={this.handleChange}
+                         //onChange={this.handleChange}
                   />
+                  <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                  </div>
 
-                  <MDBInput
-                     value={this.state.password}
+                  <div>
+                  <Field
+                     //value={this.state.password}
                      name="password"
                      label="Your password"
                      icon="lock"
                      type="password"
                      id="password"
-                     className="form-control"
-                     onChange={this.changeHandler}
-                     required
-                    //onChange={this.handleChange}
+                     className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')}
+                     //onChange={this.handleChange}
                   />
+                  <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                  </div>
 
-                  <MDBInput
-                   value={this.state.confirmpassword}
+                  <Field
+                   //value={this.state.confirmpassword}
                    name="confirmpassword"
                    id="confirm_password"
                    label="Confirm Password"
                    icon="lock"
                    type="password"
                    autoComplete="new-password"
-                   className="form-control"
-                    onChange={this.changeHandler}
-                    required
-                    //onChange={this.handleChange}
+                   className={'form-control' + (errors.confirmpassword && touched.confirmpassword ? ' is-invalid' : '')}
+                   //onChange={this.handleChange}
                   />
+                  <ErrorMessage name="confirmpassword" component="div" className="invalid-feedback" />
                   <div className="text-center mt-4 black-text">
-                    <MDBBtn color="primary" type="submit" enabled={isInvalid}>
+                    <button color="primary" type="submit" enabled={isInvalid}>
                       Sign Up
-                    </MDBBtn>
+                    </button>
                     <div className="center red-text">
                       {authError ? <p>{authError}</p> : null}
                     </div>
                   </div>
-                </form>
+                </Form>
+
+                )}
+            />
                 <div className="text-center mt-4">
                   <hr className="hr-dark" />
                   <div className="text-center d-flex justify-content-center white-label">
