63d13975686436181cfb838c017e6f8d8428a1a3
Abel Guevara <abel-alejandro.guevara-sanchez@stud.uni-bamberg.de>
2019-07-25 13:12:31 +0200

    file-sharing show list of files (1)

--- src/components/chat/FileSharing/styles.js
+++ src/components/chat/FileSharing/styles.js
@@ -28,6 +28,23 @@ const styles = theme => ({
     color: 'white',
     padding: '10px',
     boxSizing: 'border-box'
+  },
+  table: {
+    fontFamily: 'arial, sans-serif',
+    borderCollapse: 'collapse',
+    width: '100%',
+  },
+  
+  td: {
+    border: '1px solid #dddddd',
+    textAlign: 'left',
+    padding: '8px'
+  },
+  
+  th: {
+    border: '1px solid #dddddd',
+    textAlign: 'left',
+    padding: '8px'
   }
 });
 
